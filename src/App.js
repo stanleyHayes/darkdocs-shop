b29137a66ced29b2ab797eb3e0258da30a75c870
Stanley Hayford <hayfordstanley@gmail.com>
2021-07-29 20:28:14 +0000

    Added protected route to routes that need token

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React, {useEffect} from "react";
-import {Route, Switch, useHistory,} from 'react-router-dom';
+import React from "react";
+import {Route, Switch,} from 'react-router-dom';
 import DashboardPage from "./pages/dashboard/dashboard-page";
 import ProfilePage from "./pages/profile/profile-page";
 import OrdersPage from "./pages/orders/orders-page";
@@ -15,30 +15,21 @@ import ResetPasswordPage from "./pages/authentication/reset-password-page";
 import ChequesPage from "./pages/cheques/cheques-page";
 import FundsPage from "./pages/funds/funds-page";
 import ProductsPage from "./pages/products/products-page";
-import {useSelector} from "react-redux";
+import ProtectedRoute from "./components/shared/protected-route";
 
 function App() {
-
-    const history = useHistory();
-    const {loading, token} = useSelector(state => state.auth);
-
-    useEffect(() => {
-        if (!loading && !token) {
-            history.push('/auth/login');
-        }
-    }, [loading, history, token]);
 
     return (
         <ScrollToTop>
             <Switch>
-                <Route exact={true} path="/" component={DashboardPage}/>
-                <Route exact={true} path="/profile" component={ProfilePage}/>
-                <Route exact={true} path="/orders" component={OrdersPage}/>
-                <Route exact={true} path="/products" component={ProductsPage}/>
-                <Route exact={true} path="/funds" component={FundsPage}/>
-                <Route exact={true} path="/cheques" component={ChequesPage}/>
-                <Route exact={true} path="/edit-profile" component={EditProfilePage}/>
-                <Route exact={true} path="/auth/change-password" component={ChangePasswordPage}/>
+                <ProtectedRoute exact={true} path="/" component={DashboardPage}/>
+                <ProtectedRoute exact={true} path="/profile" component={ProfilePage}/>
+                <ProtectedRoute exact={true} path="/orders" component={OrdersPage}/>
+                <ProtectedRoute exact={true} path="/products" component={ProductsPage}/>
+                <ProtectedRoute exact={true} path="/funds" component={FundsPage}/>
+                <ProtectedRoute exact={true} path="/cheques" component={ChequesPage}/>
+                <ProtectedRoute exact={true} path="/edit-profile" component={EditProfilePage}/>
+                <ProtectedRoute exact={true} path="/auth/change-password" component={ChangePasswordPage}/>
                 <Route exact={true} path="/auth/forgot-password" component={ForgotPasswordPage}/>
                 <Route exact={true} path="/auth/verify-account" component={VerifyAccountPage}/>
                 <Route exact={true} path="/auth/register" component={RegisterPage}/>
